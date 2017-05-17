01e21b7b2982de349ea0fa967ad12fcc82a993ed
sky-ADMIN <546485299@qq.com>
2017-05-17 09:38:58 +0800

    添加创建直播功能

---
+++ server/models/live.js
@@ -0,0 +1,13 @@
+'use strict';
+var crypto = require('crypto');
+
+module.exports = function(Live) {
+
+  Live.observe('before save', function (ctx, next) {
+    console.log('Save a Live instance: ' + ctx.instance);
+    var data = new Date().getTime();
+    var id = crypto.createHash('md5').update(ctx.instance.name + data).digest('hex');
+    ctx.instance.rtmpStream = 'rtmp://push.lihuanyu.com/live/' + id;
+    next();
+  })
+};
