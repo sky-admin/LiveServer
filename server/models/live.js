13022b4154756b5956120e5df159107589183867
sky-ADMIN <546485299@qq.com>
2017-06-14 11:09:56 +0800

    update

--- server/models/live.js
+++ server/models/live.js
@@ -4,7 +4,8 @@ var crypto = require('crypto');
 module.exports = function(Live) {
 
   Live.observe('before save', function (ctx, next) {
-    console.log('Save a Live instance: ' + ctx.instance);
+    // console.log('Save a Live instance: ' + ctx.instance.clientId);
+    // var clientId = ctx.instance.clientId;
     var data = new Date().getTime();
     var id = crypto.createHash('md5').update(ctx.instance.name + data).digest('hex');
     ctx.instance.rtmpStream = 'rtmp://push.lihuanyu.com/live/' + id;
